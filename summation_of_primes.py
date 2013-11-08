d264874f35e0d7ecb2d63d741679f7550703e4c2
Jonathan Lebron <jlebron2@gmail.com>
2013-11-08 15:24:23 -0500

    Create summation_of_primes.py

---
+++ summation_of_primes.py
@@ -0,0 +1,26 @@
+# Author: Jonathan Lebron
+# Date: 11/8/2013
+# Description:
+#     The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#     Find the sum of all the primes below two million.
+#     - From http://projecteuler.net/problem=10
+
+import math, time
+
+num = 2000000
+start =  time.clock()
+total = (num-1) * num / 2 # add all numbers
+total -= 1 # subtract 1 since it is not prime
+primes = [1]*num
+
+m = int(math.sqrt(num))+1
+for i in xrange(2, m):
+    if primes[i] == 1:
+        for j in xrange(i*2, num, i):
+            if primes[j] == 1:
+                primes[j] = 0
+                total -= j
+
+end = time.clock() - start
+print "answer is: ", total
+print "took ", end, " seconds"
